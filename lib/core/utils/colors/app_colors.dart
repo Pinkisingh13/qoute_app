46b9091ae9250b34d4cd9a1ec683c8aee9f8422a
pinki singh <web.pinkisingh@gmail.com>
2025-04-13 06:02:51 +0530

    ADD✨: Folder structure and images

---
+++ lib/core/utils/colors/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors{
+  static Color primary = Color(0xffF9761A);
+  static Color secondary = Color(0xffFFF2E6);
+  static Color light = Color(0xffFFFFFF);
+  static Color lightOrange = Color(0xfffaa76b);
+
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:qoute_app/core/utils/themes/app_theme.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,9 +11,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Quote App',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
+      theme: AppTheme.lightTheme,
+      darkTheme: AppTheme.darkTheme,
+      // themeMode: ThemeMode.system,
+
     );
   }
 }
